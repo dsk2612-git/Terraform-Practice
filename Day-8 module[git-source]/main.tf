92bb0655543b7dea793a13046b7f82e724d50cfc
krishna <saiaws2612@gmail.com>
2025-03-27 20:06:37 +0530

    modules

--- Day-8 module[git-source]/main.tf
+++ Day-8 module[git-source]/main.tf
@@ -1,3 +1,9 @@
 provider "ams" { 
 }
 
+module "test" {
+  source = "https://github.com/dsk2612-git/Terraform-Practice/Day-8%20Modules%5Bex%EF%80%BA2%5D"
+  ami_id = "ami-0b7207e48d1b6c06f"
+  instance_type = "t2.micro"
+  key_name = "democp"
+}
